8cfbbc708185e7da7162b03f79e5c7ff0847da6c
eeuih <neuih2@gmail.com>
2023-02-11 16:01:53 +0900

    dir setting

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 5000;
+
+let corsOption = {
+  origin: 'http://localhost:3000', // 허락하는 요청 주소
+  credentials: true,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  optionsSuccessStatus: 200, // true로 하면 설정한 내용을 response 헤더에 추가 해줍니다.
+};
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors(corsOption));
+
+app.listen(port, () => {
+  console.log(`app listening at port : ${port}`);
+});
