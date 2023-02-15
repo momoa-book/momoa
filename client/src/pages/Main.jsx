75f39127fc42e4c1dee47a65ea3754352949b120
hyonie <dev.hyonie@gmail.com>
2023-02-15 23:38:22 +0900

    feat: main Description Component update

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Description from '../components/Description';
 import Header from '../components/Header';
 import Momoa from '../components/Momoa';
 import Toggle from '../components/Toggle';
@@ -8,13 +9,10 @@ export default function Main() {
   return (
     <>
       <Header />
-      {/* <Link to="/">메인 </Link>
-      <Link to="/signup">회원가입 </Link>
-      <Link to="/login">로그인 </Link>
-      <Link to="/account">가계부 </Link>
-      <Toggle /> 컴포넌트 페이지 이동용 테스트 버튼과 다크모드 토글버튼이에요
-      ~~~ */}
       <Momoa />
+      <div className="my-[130px]">
+        <Description />
+      </div>
     </>
   );
 }
