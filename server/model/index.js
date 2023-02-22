403eb845595786876c94169a75b8f9c0e2cde611
nyondoo <ahfl1129@gmail.com>
2023-02-23 01:28:58 +0900

    feat: DB모델 설정

--- server/model/index.js
+++ server/model/index.js
@@ -23,6 +23,9 @@ module.exports = db;
 db.Sheet = require('./Sheet')(sequelize, Sequelize);
 module.exports = db;
 
+db.DBhub = require('./DBhub')(sequelize, Sequelize);
+module.exports = db;
+
 // db.User.hasMany(db.Sheet, {
 //   foreignKey: 'user_email', //Sheet 테이블에있는 거
 //   sourceKey: 'user_email', //User테이블에있는 user_id
@@ -32,14 +35,14 @@ module.exports = db;
 
 //user테이블하고 sheet테이블의 관계설정
 
-db.User.hasMany(db.Sheet, {
+db.User.hasMany(db.DBhub, {
   foreignKey: 'user_email', //Sheet 테이블에있는 거
   sourceKey: 'user_email', //User테이블에있는 user_id
   onDelete: 'cascade',
   onUpdate: 'cascade',
 });
 
-db.Sheet.belongsTo(db.User, {
+db.DBhub.belongsTo(db.User, {
   foreignKey: 'user_email', // sheet테이블의 foreignkey
   targetKey: 'user_email', // user테이블의 user_id
   onDelete: 'cascade',
@@ -48,15 +51,32 @@ db.Sheet.belongsTo(db.User, {
 
 //sheet테이블하고 info테이블의 관계설정
 
-db.Sheet.hasOne(db.Info, {
-  foreignKey: 'sheet_id', //Info 테이블에있는 거
-  sourceKey: 'sheet_id', //Sheet테이블에있는 sheet_id
+db.Sheet.hasMany(
+  db.Info,
+  {
+    foreignKey: 'sheet_id', //Info 테이블에있는 거
+    sourceKey: 'sheet_id', //Sheet테이블에있는 sheet_id
+    onDelete: 'cascade',
+    onUpdate: 'cascade',
+  },
+  db.DBhub,
+  {
+    foreignKey: 'sheet_id', //Info 테이블에있는 거
+    sourceKey: 'sheet_id', //Sheet테이블에있는 sheet_id
+    onDelete: 'cascade',
+    onUpdate: 'cascade',
+  }
+);
+
+db.Info.belongsTo(db.Sheet, {
+  foreignKey: 'sheet_id', // info테이블의 foreignkey
+  targetKey: 'sheet_id', // sheet테이블의 sheet_id
   onDelete: 'cascade',
   onUpdate: 'cascade',
 });
 
-db.Info.belongsTo(db.Sheet, {
-  foreignKey: 'sheet_id', // info테이블의 foreignkey
+db.DBhub.belongsTo(db.Sheet, {
+  foreignKey: 'sheet_id', // DBhubo테이블의 foreignkey
   targetKey: 'sheet_id', // sheet테이블의 sheet_id
   onDelete: 'cascade',
   onUpdate: 'cascade',
