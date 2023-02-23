59b22b0aea0aea4935c5f09f8e33aa09f40f013b
nyondoo <ahfl1129@gmail.com>
2023-02-23 22:00:19 +0900

    feat: 정보 뿌려주기

--- server/model/index.js
+++ server/model/index.js
@@ -51,23 +51,12 @@ db.DBhub.belongsTo(db.User, {
 
 //sheet테이블하고 info테이블의 관계설정
 
-db.Sheet.hasMany(
-  db.Info,
-  {
-    foreignKey: 'sheet_id', //Info 테이블에있는 거
-    sourceKey: 'sheet_id', //Sheet테이블에있는 sheet_id
-    onDelete: 'cascade',
-    onUpdate: 'cascade',
-  },
-  db.DBhub,
-  {
-    foreignKey: 'sheet_id', //Info 테이블에있는 거
-    sourceKey: 'sheet_id', //Sheet테이블에있는 sheet_id
-    onDelete: 'cascade',
-    onUpdate: 'cascade',
-  }
-);
-
+db.Sheet.hasMany(db.Info, {
+  foreignKey: 'sheet_id', //Info 테이블에있는 거
+  sourceKey: 'sheet_id', //Sheet테이블에있는 sheet_id
+  onDelete: 'cascade',
+  onUpdate: 'cascade',
+});
 db.Info.belongsTo(db.Sheet, {
   foreignKey: 'sheet_id', // info테이블의 foreignkey
   targetKey: 'sheet_id', // sheet테이블의 sheet_id
@@ -75,6 +64,12 @@ db.Info.belongsTo(db.Sheet, {
   onUpdate: 'cascade',
 });
 
+db.Sheet.hasMany(db.DBhub, {
+  foreignKey: 'sheet_id', //Info 테이블에있는 거
+  sourceKey: 'sheet_id', //Sheet테이블에있는 sheet_id
+  onDelete: 'cascade',
+  onUpdate: 'cascade',
+});
 db.DBhub.belongsTo(db.Sheet, {
   foreignKey: 'sheet_id', // DBhubo테이블의 foreignkey
   targetKey: 'sheet_id', // sheet테이블의 sheet_id
