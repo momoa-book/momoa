959c099c6e5dd9022123fcada86dedfeec6ef5a3
nyondoo <ahfl1129@gmail.com>
2023-03-21 20:03:17 +0900

    feat: api RESTful한 주소로 수정

--- server/routes/Index.js
+++ server/routes/Index.js
@@ -9,14 +9,14 @@ const controllerMain = require('../controller/Cmainleft');
 const controllerData = require('../controller/CSheetData');
 
 //이메일 인증요청
-router.post('/check_email', controllerSignUp.send_code);
+router.post('/check-email', controllerSignUp.send_code);
 
 //회원가입
 router.post('/signup', controllerSignUp.approve_code);
 router.post('/signup2', controllerSignUp.finish_signup);
 
 //카카오 로그인
-router.post('/getkakao', controllerKakao.KakaoLogin);
+router.post('/kakao', controllerKakao.KakaoLogin);
 
 //로그인
 router.post('/signin', controllerSignIn.user_signin);
@@ -39,21 +39,21 @@ router.get('/verify', controllerRefreshToken.verifyToken);
 //마이페이지 or 캘린더화면 처럼 유저 정보뿌려주는 화면에서 요청할 api
 
 //차트 정보 불러오기
-router.get('/getsheetdata', controllerData.getsheetdata);
+router.get('/sheetdata', controllerData.getsheetdata);
 
-router.get('/getcalendar', controllerData.getcalendardata);
+router.get('/calendar', controllerData.getcalendardata);
 
 //sheet_id가져오는
-router.get('/getsheetid', controllerMain.get_sheetid);
+router.get('/sheetid', controllerMain.get_sheetid);
 
 //개인정보가져오는
-router.get('/getpersonalinfo', controllerMain.get_personalinfo);
+router.get('/personalinfo', controllerMain.get_personalinfo);
 
 //수입지출등등입력하는 왼쪽 상단부
 router.post('/writeinfo', controllerMain.write_info);
 
 //목표금액 불러오는
-router.get('/getgoal', controllerMain.get_goal);
+router.get('/goal', controllerMain.get_goal);
 
 //목표금액 입력하는
 router.post('/writegoal', controllerMain.write_goal);
